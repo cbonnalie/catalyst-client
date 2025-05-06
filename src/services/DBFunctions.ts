004b6b9bb92240897b51ea713db47e666fbe7cbf
christian <christianbonnalie@gmail.com>
2025-05-06 00:44:01 -0700

    DBFunctions.ts prefix

--- src/services/DBFunctions.ts
+++ src/services/DBFunctions.ts
@@ -9,7 +9,8 @@
 
 export async function fetchEvents(x: number): Promise<Event[]> {
   try {
-    const prefix = import.meta.env.VITE_SERVER_URL;
+    const prefix = String(import.meta.env.VITE_SERVER_URL);
+    console.log(`Prefix: ${prefix}`);
     const url = `${prefix}/api/xEvents?x=${x}`;
     console.log(`Fetching events from ${url}`);
     const response = await fetch(url);
