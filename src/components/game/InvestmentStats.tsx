e45ae26386c84bdf4e8a4f05aeafaf68a2ee206c
christian <christianbonnalie@gmail.com>
2025-05-14 17:49:43 -0700

    area chart outlines

--- src/components/game/InvestmentStats.tsx
+++ src/components/game/InvestmentStats.tsx
@@ -21,7 +21,7 @@ export const InvestmentStats: React.FC<InvestmentStatsProps> = (
     return (
         isMobile
             ? (
-                <Grid container spacing={1} sx={{height: "100%", padding: "20px 0"}}>
+                <Grid container spacing={1} sx={{height: "100%", padding: "0"}}>
                     <Grid size={12} sx={{height: "100%"}}>
                         <Box sx={{display: "flex", flexDirection: "column", height: "100%", maxHeight: "100%", overflow: "hidden"}}>
                             {/* this box is ruining my life */}
